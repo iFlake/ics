d58df37c502a6ddb1a431d2dd5e692ce1716a0ae
iFlake <iceflake@cloudwards.es>
2017-06-10 18:07:54 +0400

    Execute application

--- index.php
+++ index.php
@@ -5,3 +5,6 @@ define("ics_root", dirname(__FILE__));
 define("ics_internal", dirname(__FILE__) . "/internal");
 
 include ics_internal . "/ics/include.php";
+
+$application = new \itais\ics\application\Application;
+$application->Execute();
