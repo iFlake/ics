bb7ed63665e2739ce7de5f7ffdaa6334716a3f85
iFlake <iceflake@cloudwards.es>
2017-06-10 21:44:12 +0400

    Dummy mysqli configuration

---
+++ internal/ics/config/mysqli.php
@@ -0,0 +1,11 @@
+<?php
+namespace itais\ics\config;
+
+class MySQLi
+{
+    public const host        = "";
+    public const username    = "";
+    public const password    = "";
+    public const database    = "";
+    public const port        = 0;
+}
