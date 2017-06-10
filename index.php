61a286ea4a167db0415a8d75f82d8be51c98238c
iFlake <iceflake@cloudwards.es>
2017-06-10 18:17:42 +0400

    Add execution context

--- index.php
+++ index.php
@@ -6,5 +6,10 @@ define("ics_internal", dirname(__FILE__) . "/internal");
 
 include ics_internal . "/ics/include.php";
 
-$application = new \itais\ics\application\Application;
-$application->Execute();
+class ExecutionContext
+{
+    public static $application = null;
+}
+
+ExecutionContext::$application = new \itais\ics\application\Application;
+ExecutionContext::$application.Execute();
