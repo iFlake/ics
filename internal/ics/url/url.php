8b511d795f7f0e7ca50cae373440710ca3f045da
iFlake <iceflake@cloudwards.es>
2017-06-10 18:17:36 +0400

    Complete URL parser

--- internal/ics/url/url.php
+++ internal/ics/url/url.php
@@ -17,6 +17,25 @@ class URL
 
         self::$sections     = array_slice(self::$sections, count($match));
 
-        
+        if (count(self::$sections) < 1) return self::ContinueParse(true);
+        else
+        {
+            if (array_key_exists(self::$sections[0], \itais\ics\ExecutionContext::$Application->extensions_raw) == true) return self::ContinueParse(false);
+            else return self::ContinueParse(true);
+        }
+    }
+
+    protected static function ContinueParse($default_extension)
+    {
+        if ($default_extension == true)
+        {
+            self::$extension     = \itais\ics\ExecutionContext::$Application->GetDefaultExtension();
+            self::$parameters    = self::$sections;
+        }
+        else
+        {
+            self::$extension     = self::$sections[0];
+            self::$parameters    = array_slice(self::$sections, 1);
+        }
     }
 }
